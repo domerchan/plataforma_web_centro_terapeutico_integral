4b88cae18be6c5ea5de0028459212fae7139c8ba
Domenica Merchan <domerchan@hotmail.com>
2020-07-26 02:44:22 -0500

    first commit

---
+++ website_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\domenica\onedrive\ups\8vo\extensiones\django\centro_website\website_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

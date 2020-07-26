1d8d804bd5ae52125a99aa6f94d166818a71b0bd
Domenica Merchan <domerchan@hotmail.com>
2020-07-26 03:10:45 -0500

    se agrega template kiddos

--- centro_website/website/urls.py
+++ centro_website/website/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about.html', views.about, name='about'),
+    path('blog.html', views.blog, name='blog'),
+    path('blog-single.html', views.blogsingle, name='blogsingle'),
+    path('contact.html', views.contact, name='contact'),
+    path('courses.html', views.courses, name='courses'),
+    path('pricing.html', views.pricing, name='pricing'),
+    path('teacher.html', views.teacher, name='teacher'),
 ]
--- centro_website/website/views.py
+++ centro_website/website/views.py
@@ -4,4 +4,25 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hello")
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def blogsingle(request):
+    return render(request, 'blog-single.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def courses(request):
+    return render(request, 'courses.html')
+
+def pricing(request):
+    return render(request, 'pricing.html')
+
+def teacher(request):
+    return render(request, 'teacher.html')

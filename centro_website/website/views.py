2318335ffb4a796d925767cc975b0435e3b8e9dd
Alejandro Enriquez <AlejoEnriquez2>
2020-08-27 02:33:00 -0500

    Formulario de registro funcionando
    Falta validaciones y completar login

--- centro_website/website/views.py
+++ centro_website/website/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from website.models import User
+from website.models import Direction
+from website.forms import UserForm
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -27,6 +31,12 @@ def index(request):
 def information(request):
     return render(request, 'information.html')
 
+def login(request):
+    return render(request, 'login.html')
+
+def registro(request):
+    return render(request, 'registro.html')
+
 def report(request):
     return render(request, 'report.html')
 
@@ -41,3 +51,35 @@ def training(request):
 
 def tutorial(request):
     return render(request, 'tutorial.html')
+
+def registrar(request):
+    if request.method=="POST":
+        nombres = request.POST["names"]
+        apellidos = request.POST["lnames"]
+        cedula = request.POST["id"]
+        tel1 = request.POST["tel1"]
+        tel2 = request.POST["tel2"]
+        dir1 = request.POST["dir1"]
+        dir2 = request.POST["dir2"]
+        dir3 = request.POST["dir3"]
+        dir4 = request.POST["dir4"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        birth = request.POST["birth"]
+        sex = request.POST["sex"]
+        status = request.POST["status"]
+
+        rol = request.POST["rol"]
+        job = request.POST["job"]
+        degree = request.POST["degree"]
+        bio = request.POST["bio"]
+        
+        direccion=Direction(main_street=dir1, secondary_street=dir2, house_number=dir3, reference=dir4) 
+        direccion.save()
+        usuario=User(first_name=nombres, last_name=apellidos, identity_card=cedula, rol=rol, birth=birth, sex=sex, civil_status=status, phone_number_1=tel1, phone_number_2=tel2, email=email, direction=direccion, job_title=job, degree=degree, biography=bio)
+        usuario.save()
+
+        response = redirect('login')
+        return response
+    else:
+        return render(request, "index.html")

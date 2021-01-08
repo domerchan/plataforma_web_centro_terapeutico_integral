b338f1843144ea9111f87db802f0ab16102c19f5
Do Merchán <34308597+domerchan@users.noreply.github.com>
2021-01-08 01:08:19 -0500

    new pc

--- website_env/Scripts/activate.bat
+++ website_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Domenica\OneDrive\UPS\8vo\Extensiones\Django\centro_website\website_env
+set VIRTUAL_ENV=C:\Users\domer\OneDrive\UPS\8vo\Extensiones\Django\centro_website\website_env
 
 if not defined PROMPT set PROMPT=$P$G
 
